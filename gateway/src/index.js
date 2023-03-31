e56d2d419a7e7cf47276e1fd4e1697c18c98671b
samir mahmud <samir93bj@gmail.com>
2023-03-31 20:50:44 -0300

    chore: add folder structure gateway

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router()
 
 app.use(express.json());
+app.use(cors)
 app.use(router)
 
 router.get('/', async (req, res) => {
 	res.status(200).json({
-		message: 'Customer Service: Success up server'
+		message: 'Product Service: Success up server'
 	})	
 })
 
-app.listen(8002, () => {
-	console.log('Customer is listening on http://localhost:8002')
+app.listen(8005, () => {
+	console.log('Customer is listening on http://localhost:8005')
 })
--- products/src/index.js
+++ products/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router()
+
 app.use(express.json());
 
-const router = express.Router()
 app.use(router)
 
 router.get('/', async (req, res) => {
