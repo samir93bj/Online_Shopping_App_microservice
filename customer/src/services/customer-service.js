54bd4b5f5817d55608c0180bba0d3d918c4bd010
samir mahmud <samir93bj@gmail.com>
2023-04-01 01:35:58 -0300

    refactor [customer]: refactor method service signup

--- customer/src/services/customer-service.js
+++ customer/src/services/customer-service.js
@@ -11,25 +11,21 @@ class CustomerService {
     }
 
     async SignIn(userInputs){
-
-        const { email, password } = userInputs;
-        
         try {
-            
-            const existingCustomer = await this.repository.FindCustomer({ email});
+					const { email, password } = userInputs;
+          const existingCustomer = await this.repository.FindCustomer({ email});
 
-            if(existingCustomer){
+          if(!existingCustomer) 
+						return FormateData(null);
             
-                const validPassword = await ValidatePassword(password, existingCustomer.password, existingCustomer.salt);
+          const validPassword = await ValidatePassword(password, existingCustomer.password, existingCustomer.salt);
                 
-                if(validPassword){
-                    const token = await GenerateSignature({ email: existingCustomer.email, _id: existingCustomer._id});
-                    return FormateData({id: existingCustomer._id, token });
-                } 
-            }
-    
-            return FormateData(null);
-
+          if(validPassword)
+						return FormateData(null);
+            
+					const token = await GenerateSignature({ email: existingCustomer.email, _id: existingCustomer._id});
+          return FormateData({id: existingCustomer._id, token });
+					
         } catch (err) {
             throw new APIError('Data Not found', err)
         }
