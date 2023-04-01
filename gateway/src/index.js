7610319727065c13a82f43f1fc8a4d23a7f19001
samir mahmud <samir93bj@gmail.com>
2023-03-31 21:12:16 -0300

    feat: add proxy config

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const proxy = require('express-http-proxy');
 
 const app = express();
-const router = express.Router()
 
+app.use(cors())
 app.use(express.json());
-app.use(cors)
-app.use(router)
 
-router.get('/', async (req, res) => {
-	res.status(200).json({
-		message: 'Product Service: Success up server'
-	})	
-})
+app.use('/customer', proxy('http://localhost:8002'))
+app.use('/shopping', proxy('http://localhost:8004'))
+app.use('/', proxy('http://localhost:8003'))
 
-app.listen(8005, () => {
-	console.log('Customer is listening on http://localhost:8005')
+app.listen(8000, () => {
+	console.log('Gateway is listening on http://localhost:8000')
 })
