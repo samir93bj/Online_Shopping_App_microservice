81f02e89503cc6be2e723507a0f6e9171ee14477
samir mahmud <samir93bj@gmail.com>
2023-04-02 21:11:16 -0300

    refactor [product]: add event addproductcart and deleteProductCart

--- products/src/api/products.js
+++ products/src/api/products.js
@@ -92,10 +92,18 @@ module.exports = (app) => {
     app.put('/cart',UserAuth, async (req,res,next) => {
 			try {     
         const { _id } = req.user;
-        const product = await service.GetProductById(_id);
-				const result =  await customerService.ManageCart(req.user._id, product, qty, false);
+
+				const { data } = await service.GetProductPayload(_id, { productId: req.body._id, qty: req.body.qty }, 'ADD_TO_CART')
 				
-				return res.status(200).json(result);
+				PublishCustomerEvent(data);
+				PublishShoppingEvent(data);
+
+				const response = {
+					product: data.data.product,
+					unit: data.data.qty
+				}
+
+				return res.status(200).json(response);
     
       } catch (err) {
           next(err)
@@ -103,16 +111,24 @@ module.exports = (app) => {
     });
     
     app.delete('/cart/:id',UserAuth, async (req,res,next) => {
-
+			try {
         const { _id } = req.user;
+				const productId = req.params.id;
 
-        try {
-            const product = await service.GetProductById(req.params.id);
-            const result = await customerService.ManageCart(_id, product, 0 , true);             
-            return res.status(200).json(result);
-        } catch (err) {
-            next(err)
-        }
+				const { data } = await service.GetProductPayload(_id, { productId }, 'REMOVE_FROM_CART')
+
+        PublishCustomerEvent(data);
+				PublishShoppingEvent(data);
+
+				const response = {
+					product: data.data.product,
+					unit: data.data.qty
+				}
+
+				return res.status(200).json(response);
+      } catch (err) {
+        next(err)
+      }
     });
 
     //get Top products and category
