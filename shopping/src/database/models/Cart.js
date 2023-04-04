446284f0bcf4cf078fe27a160725ce78f24e7ad9
samir mahmud <samir93bj@gmail.com>
2023-04-04 00:44:38 -0300

    refactor [shopping]: changes in models order y cart

--- shopping/src/database/models/Cart.js
+++ shopping/src/database/models/Cart.js
@@ -9,9 +9,13 @@ const CartSchema = new Schema({
           product: {
 						_id: { type: String, required: true },
 						name: { type: String },
+						desc: { type: String },
 						banner: { type: String },
+						type: { type: String }, 
+						unit: { type: Number },
 						price: { type: Number },
-					} ,
+						suplier: { type: String },
+					},
 					unit: { type: Number, required: true }
         }
     ]
