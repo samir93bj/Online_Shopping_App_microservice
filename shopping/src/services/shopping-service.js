67ab0091af50dbfa394c3bb86a82b59c7f98ec63
samir mahmud <samir93bj@gmail.com>
2023-04-04 13:34:56 -0300

    feat [shopping]: add service getorderpayload

--- shopping/src/services/shopping-service.js
+++ shopping/src/services/shopping-service.js
@@ -8,7 +8,7 @@ class ShoppingService {
     this.repository = new ShoppingRepository()
   }
 
-  async getCart ({ _id }) {
+  async GetCart ({ _id }) {
     try {
       const cartItems = await this.repository.Cart(_id)
 
@@ -34,6 +34,46 @@ class ShoppingService {
       return FormateData(orders)
     } catch (err) {
       throw new APIError('Data Not found', err)
+    }
+  }
+
+  async ManageCart (customerId, item, qty, isRemove) {
+    try {
+      const cartResult = await this.repository.AddCartItem(customerId, item, qty, isRemove)
+
+      return FormateData(cartResult)
+    } catch (err) {
+      throw new APIError('Data Not found', err)
+    }
+  }
+
+  async SubscribeEvents (payload) {
+    const { event, data } = payload
+
+    const { userId, product, order, qty } = data
+
+    switch (event) {
+      case 'ADD_TO_CART':
+        this.ManageCart(userId, product, qty, false)
+        break
+      case 'REMOVE_FROM_CART':
+        this.ManageOrder(userId, order, qty, true)
+        break
+      default:
+        break
+    }
+  }
+
+  async GetOrderPayload (userId, order, event) {
+    if (order) {
+      const payload = {
+        event,
+        data: { userId, order }
+      }
+
+      return payload
+    } else {
+      return FormateData({ error: 'No Order Available' })
     }
   }
 }
